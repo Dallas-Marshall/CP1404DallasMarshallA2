5cc0fb056d3d8da1f95a8e9ba594e22b046b35eb
Dallas Marshall <dallas.marshall@my.jcu.edu.au>
2019-09-24 17:47:35 +1000

    Add __init__ and __str__ and test code

--- movie.py
+++ movie.py
@@ -1,9 +1,21 @@
-"""..."""
+"""Movie class for assignment 2."""
 
 
 # TODO: Create your Movie class in this file
 
 
 class Movie:
-    """..."""
-    pass
+    """Movie class to represent details of a movie."""
+    IS_ALL_MOVIES_WATCHED = False
+
+    def __init__(self, title="", year=0, category="", is_watched=False):
+        """Initialise a Movie."""
+        self.title = title
+        self.year = year
+        self.category = category
+        self.is_watched = is_watched
+
+    def __str__(self):
+        """Return a string representation of Guitar."""
+        return "{self.title},{self.year},{self.category},{self.is_watched}".format(self=self)
+
--- test_movie.py
+++ test_movie.py
@@ -16,8 +16,16 @@ def run_tests():
 
     # Test initial-value movie
     initial_movie = Movie("Thor: Ragnarok", 2017, "Comedy", True)
-    # TODO: Write tests to show this initialisation works
 
+    # TODO: Write tests to show this initialisation works
+    print("Test Initial Movie:")
+    print("Output:\n\tExpected: Thor: Ragnarok,2017,Comedy,True \n\tgot;      {}".format(initial_movie))
+    print("Title:\n\tExpected: Thor: Ragnarok - <class 'str'>\n\tgot;      {} - {}".format(initial_movie.title,
+                                                                                           type(initial_movie.title)))
+    print("Year:\n\tExpected: 2017 - <class 'int'>\n\tgot;      {} - {}".format(initial_movie.year,
+                                                                                type(initial_movie.year)))
+    print("Category:\n\tExpected: Comedy - <class 'str'>\n\tgot;      {} - {}".format(initial_movie.category,
+                                                                                      type(initial_movie.category)))
     # TODO: Add more tests, as appropriate, for each method
 
 
