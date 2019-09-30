dd53543ee9dd82fd09cb5a6bebb08f6b43ca9ac3
Dallas Marshall <dallas.marshall@my.jcu.edu.au>
2019-09-30 22:23:58 +1000

    Fix module docstrings

--- movie.py
+++ movie.py
@@ -5,7 +5,7 @@
 
 
 class Movie:
-    """Movie class to represent details of a movie."""
+    """Movie class to represent details of a Movie."""
 
     def __init__(self, title="", year=0, category="", is_watched=False):
         """Initialise a Movie."""
@@ -15,7 +15,7 @@ class Movie:
         self.is_watched = is_watched
 
     def __str__(self):
-        """Return a string representation of Guitar."""
+        """Return a string representation of Movie."""
         return "{self.title},{self.year},{self.category},{self.is_watched}".format(self=self)
 
     def watch_movie(self):
@@ -27,7 +27,7 @@ class Movie:
             return print("You have already watched {self.title}".format(self=self))
 
     def un_watch_movie(self):
-        """Set a movie as un-watched."""
+        """Set a Movie as un-watched."""
         if self.is_watched:
             self.is_watched = False
             return print("{self.title} from {self.year} un-watched".format(self=self))
