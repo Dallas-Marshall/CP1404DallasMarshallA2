3f384d32ca2ac9f36b73a2efea46209675cea26c
Dallas Marshall <dallas.marshall@my.jcu.edu.au>
2019-09-30 22:23:01 +1000

    Add tested init and str methods for moviecollection

--- moviecollection.py
+++ moviecollection.py
@@ -1,9 +1,14 @@
-"""..."""
-
-
-# TODO: Create your MovieCollection class in this file
+"""Movie collection class for assignment 2."""
+from movie import Movie
 
 
 class MovieCollection:
-    """..."""
-    pass
+    """A class to keep track of a list of Movie objects."""
+
+    def __init__(self):
+        """Initialise empty list to store movie objects."""
+        self.movies = []
+
+    def __str__(self):
+        """Return a string representation of movie_collection."""
+        return str([str(movie) for movie in self.movies])
